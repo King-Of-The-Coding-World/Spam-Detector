a08a509bb6adba74a0d8293f50461dfb260e5210
King-Of-The-Coding-World <jatinsose473@gmail.com>
2026-03-19 13:31:43 +0530

    Adding a condition that if msg is empty, return spam

--- spamDetector.py
+++ spamDetector.py
@@ -9,7 +9,7 @@ def checkStatus(msg):
     msg_vec = vectorizer.transform([msg])
     prediction = model.predict(msg_vec)
 
-    if prediction[0] == 1 or prediction[0] == 2:
+    if msg == "" or prediction[0] == 1 or prediction[0] == 2:
         return "Spam"
     else:
         return "Not Spam"
